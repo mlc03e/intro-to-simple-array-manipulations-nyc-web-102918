95d18530adef7332302817071f00d9d2768a9707
Meghan Campbell <mlc03e@gmail.com>
2018-10-19 22:14:35 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -24,8 +24,7 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
-  ice_cream_brands.shift(2)
+  array.shift(2)
 end
 
 def using_concat(arr_1, arr_2)
